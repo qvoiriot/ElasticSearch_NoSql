9d7dfb0882404bfb8e9f757e959dbe3587aa980c
qvoiriot <qvoiriot@gmail.com>
2014-06-24 22:43:42 +0200

    fix the foreach exec and read files for livesearch

--- launcher.php
+++ launcher.php
@@ -8,14 +8,14 @@ $params = array();
 $params['index'] = 'epsi';
 $params['type']  = 'memories';
 
-$pdfArray = array("/ElasticSearch_NoSql/memories/M13_LEROUX.pdf", "/ElasticSearch_NoSql/memories/M13_LEAUTE.pdf");
+$pdfArray = array("/ElasticSearch_NoSql/memories/M13_LEROUX.pdf", "/ElasticSearch_NoSql/memories/M13_LEAUTE.pdf", "/ElasticSearch_NoSql/memories/M13_LEZER.pdf", "/ElasticSearch_NoSql/memories/M13_NOIRAUD.pdf", "/ElasticSearch_NoSql/memories/M13_POIRIER.pdf", "/ElasticSearch_NoSql/memories/M13_TALEC.pdf", "/ElasticSearch_NoSql/memories/M13_TEYSSIER.pdf");
 
 foreach ($pdfArray as $pdf) {
   exec('/usr/local/bin/pdftotext -enc UTF-8 '.$pdf);
 
   $name = explode('.', $pdf);
   $nametxt = $name[0];
-  $bodyContent = file_get_contents($nametxt.'.txt');
+  $bodyContent .= file_get_contents($nametxt.'.txt');
 
 }
 
